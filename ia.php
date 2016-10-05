36ae926fbbde9cf871df99df6815673a64c8d925
Sandro Comes Zacares <sacoza@floridauniversitaria.es>
2016-10-05 12:45:18 +0200

    first commit

---
+++ ia.php
@@ -0,0 +1,23 @@
+<?php
+/**
+
+ */
+class ia
+{
+
+  // Declaración de una propiedad
+  public $fila = '3';
+  public $columna = '1';
+
+  // Declaración de un método
+  public function getfila() {
+      $this->fila = rand (1,3);
+      return $this->fila;
+  }
+  public function getcolumna() {
+      $this->columna = rand (1,3);
+      return $this->columna;
+  }
+}
+
+?>
